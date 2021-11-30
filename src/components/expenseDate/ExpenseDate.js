16ba89b1caadbc9d76fa88937eb5c4774bd9edac
georgiosan <georgiosanagnostou@hotmail.com>
2021-11-30 15:19:16 +0200

    Changed the month and day format

--- src/components/expenseDate/ExpenseDate.js
+++ src/components/expenseDate/ExpenseDate.js
@@ -1,12 +1,12 @@
 export default function ExpenseDate(props) {
   const year = props.date.toLocaleString('en-US', { year: 'numeric' });
-  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const month = props.date.toLocaleString('en-US', { month: 'short' });
   const day = props.date.toLocaleString('en-US', { day: 'numeric' });
 
   return (
     <div className="expense-date">
       <div>{month}</div>
-      <div>{day}</div>
+      <div className="expense-date-day">{day}</div>
       <div>{year}</div>
     </div>
   );
