b632c90aaabf219fc479a850391319bf3615b115
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-02 23:19:50 +0200

    Configured expenses to automatically render new entries

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,17 +1,31 @@
 import ExpenseItem from '../expenseItems/ExpenseItem';
 import Card from '../card/Card';
+import ExpenseFilter from '../expenseFilter/ExpenseFilter';
+import { useState } from 'react/cjs/react.development';
 
 export default function Expenses(props) {
+  const [selectedFilter, setSelectedFilter] = useState('default');
+  const filterChangeHandler = (selection) => {
+    setSelectedFilter(selection);
+  };
+
   return (
-    <Card className="expenses">
-      {props.items.map((item) => (
-        <ExpenseItem
-          key={item.id}
-          title={item.title}
-          date={item.date}
-          amount={item.amount}
+    <div className="app-wrapper">
+      <Card className="expenses">
+        <ExpenseFilter
+          selected={selectedFilter}
+          onFilterChange={filterChangeHandler}
         />
-      ))}
-    </Card>
+
+        {props.items.map((item) => (
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            date={item.date}
+            amount={item.amount}
+          />
+        ))}
+      </Card>
+    </div>
   );
 }
