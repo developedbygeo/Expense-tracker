a2abe057deb3058c3ecaec93dab3881cbbc975bd
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-03 20:34:54 +0200

    Readjusted the title to be derived from the passed-down props

--- src/components/expenseItems/ExpenseItem.js
+++ src/components/expenseItems/ExpenseItem.js
@@ -6,7 +6,7 @@ const ExpenseItem = (props) => {
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2 className="yolo">{props.title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
     </Card>
