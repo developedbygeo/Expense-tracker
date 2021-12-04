cbeeb7c3183711baf9294d392848ee40241a0753
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-04 03:06:17 +0200

    Wrapped the returned JSX in a list item

--- src/components/expenseItems/ExpenseItem.js
+++ src/components/expenseItems/ExpenseItem.js
@@ -3,13 +3,15 @@ import Card from '../card/Card';
 
 const ExpenseItem = (props) => {
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 };
 export default ExpenseItem;
